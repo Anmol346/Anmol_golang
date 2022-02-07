160b918bf211ad109a70ccefb6cf2f9ed9484179
Anmol Barhate <67816559+Anmol346@users.noreply.github.com>
2022-02-07 13:43:59 +0530

    Update view.go

--- view.go
+++ view.go
@@ -3,3 +3,18 @@ package models
 type View struct {
 	Todos []Todo
 }
+
+
+slicee:=[]string{"Netflix","Metaversr","Amzone","another life"}
+
+for index,err:=range slicee{
+	
+	if err !=nil{
+		panic("something went wrong")
+	}
+	fmt.Println("The data of the slice is as follow")
+	slicee=append(slicee[:index+1],slicee[index+1:]...)
+}
+
+fmt.Println(slicee)
+}
